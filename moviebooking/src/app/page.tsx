18f2eaf8645ea2223bfbbc124bc31a27b9b1127e
Code-Ranger28 <149793157+Code-Ranger28@users.noreply.github.com>
2025-03-15 08:40:55 +0530

    Update page.tsx

--- moviebooking/src/app/page.tsx
+++ moviebooking/src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Image from "next/image";
 import styles from "./page.module.css";
 import HomeSlider from "@/components/Navbar/HomeSlider/HomeSlider";
 import MovieCarousel from "@/components/Navbar/MovieCarousel/MovieCarousel";
