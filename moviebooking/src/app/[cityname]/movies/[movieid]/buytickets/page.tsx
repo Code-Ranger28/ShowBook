c522e28df521cc7e417a8ef7b8351731b3aa1da5
Code-Ranger28 <149793157+Code-Ranger28@users.noreply.github.com>
2025-03-15 13:28:14 +0530

    Update page.tsx

--- moviebooking/src/app/[cityname]/movies/[movieid]/buytickets/page.tsx
+++ moviebooking/src/app/[cityname]/movies/[movieid]/buytickets/page.tsx
@@ -5,7 +5,7 @@ import "react-date-picker/dist/DatePicker.css";
 import "react-calendar/dist/Calendar.css";
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
-import styles from "./BuyTicketsPage.module.css";
+import "./BuyTicketsPage.css";
 
 // Define types for movie and theatre
 interface Movie {
@@ -73,7 +73,7 @@ const Page = () => {
   return (
     <>
       {movie && (
-        <div className={styles.buytickets}>
+        <div className=buytickets>
           <div className="s1">
             <div className="head">
               <h1>
